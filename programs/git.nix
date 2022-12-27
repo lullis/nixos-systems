009efbed2560c34be4d582c3faa740793c606062
Raphael Lullis <raphael@mushroomlabs.com>
2022-12-26 23:15:58 -0300

    Add configuration for git

---
+++ programs/git.nix
@@ -0,0 +1,15 @@
+{
+  enable = true;
+
+  userName = "Raphael Lullis";
+  userEmail = "raphael@mushroomlabs.com";
+
+  aliases = {
+    st = "status";
+    ci = "commit";
+    co = "checkout";
+    w = "whatchanged";
+  };
+
+
+}
