23a640fd91f9e853f5a172ada50212f2c2045ff9
Raphael Lullis <raphael@mushroomlabs.com>
2025-05-02 01:26:41 +0200

    Reintroduce docker-proxy

--- roles/workstation.nix
+++ roles/workstation.nix
@@ -65,7 +65,7 @@ in
   virtualisation.docker = {
     enable = true;
     daemon.settings = {
-      userland-proxy = false;
+      # userland-proxy = false;
       experimental = true;
       metrics-addr = "0.0.0.0:9323";
       ipv6 = true;
